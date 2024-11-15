086dc71f0173824458572904bba5fab2290fbec2
MAALAL-Ines-2326071aa <ines.maalal@etu.univ-amu.fr>
2024-11-15 16:32:12 +0100

    classe creature

--- src/services_medicaux/ServiceMed.java
+++ src/services_medicaux/ServiceMed.java
@@ -2,8 +2,8 @@ package services_medicaux;
 
 public class ServiceMed {
     public String nom;
-    public int nbMax;
-    public int nbPresent;
+    public int nbMaxCreatures;
+    public int nbCreaturesPresentes;
     public int superficie;
     public String budget;
 
